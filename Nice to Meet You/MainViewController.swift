30ab044fe3837dbe776407fd49ce7aa21bbd7ad0
Ike <ike.mattice@tiger-sheep.com>
2017-01-03 15:07:30 -0800

    add additional questions.  Prevent same question and background color from occurring back to back.

--- Nice to Meet You/MainViewController.swift
+++ Nice to Meet You/MainViewController.swift
@@ -16,6 +16,14 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+ //       let button = UIButton(frame: CGRect(x: 0, y: 0, width: 110, height: 55))
+ //           button.setTitle("Next", for: .normal)
+ //           button.set
+        
+//        self.view.addSubview(button)
+        
+        
         getQuestion(previousQuestion: nil)
         setBackgroundColor(previousColor: nil)
     }
@@ -26,7 +34,7 @@ class MainViewController: UIViewController {
     }
 
     @IBAction func next(_ sender: UIButton) {
-        getQuestion(previousQuestion: self.buttonLabel.currentTitle)
+        getQuestion(previousQuestion: self.questionLabel.text)
         setBackgroundColor(previousColor: self.view.backgroundColor)
         
     }
@@ -38,31 +46,50 @@ extension MainViewController {
         let randomIndex = Int(arc4random_uniform(UInt32(questions.count)))
             
         let question = questions[randomIndex]
-        if question == previousQuestion {
+        print("question: \(question)  previous question: \(previousQuestion)")
+        if previousQuestion != nil, question == previousQuestion {
+            print("question was the same")
+            print("chosen question: \(question)  previous question: \(previousQuestion)")
             getQuestion(previousQuestion: previousQuestion)
+        } else {
+            self.updateQuestionLabel(withCurrentQuestion: question)
         }
-        
-        //let attributedString = NSAttributedString(string: question, attributes: <#T##[String : Any]?#>)
-
-        
-        self.updateQuestionLabel(withCurrentQuestion: question)
     }
 
     func setBackgroundColor(previousColor: UIColor?) {
         let randomIndex = Int(arc4random_uniform(UInt32(backgroundColors.count)))
         let color = backgroundColors[randomIndex]
         
-        if color == previousColor {
+        if previousColor != nil, color == previousColor! {
+            print("color was the same")
+            print("chosen color: \(color)  previousColor: \(previousColor)")
             setBackgroundColor(previousColor: color)
+        } else {
+            self.view.backgroundColor = color
         }
-        self.view.backgroundColor = color
+    }
+    func getBackgroundComplimentColor() -> UIColor {
+        let color = UIColor()
+        
+        return color
     }
     func updateQuestionLabel(withCurrentQuestion question: String) {
-        questionLabel.text = question
+        let textShadow = NSShadow()
+            textShadow.shadowBlurRadius = 2
+            textShadow.shadowOffset = CGSize(width: 3, height: 3)
+            textShadow.shadowColor = UIColor.gray
+
+        let attributes = [
+            NSFontAttributeName: UIFont.systemFont(ofSize: 24.0),
+            NSForegroundColorAttributeName: UIColor.white,
+            NSShadowAttributeName: textShadow
+        ]
+        let attributedQuestion = NSAttributedString(string: question, attributes: attributes)
+        questionLabel.attributedText = attributedQuestion
     }
-    func updateQuestionLabel(withCurrentQuestion question: String) {
-        questionLabel.text = question
-    }
+//    func updateQuestionLabel(withCurrentQuestion question: String) {
+//        questionLabel.text = question
+//    }
 
 }
 
--- Nice to Meet You/backgroundColors.swift
+++ Nice to Meet You/backgroundColors.swift
@@ -10,24 +10,24 @@ import UIKit
 
 
 let backgroundColors: [UIColor] = [
-    UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0),
-    UIColor(red: 192/255, green: 57/255, blue: 43/255, alpha: 1.0),
-    UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1.0),
-    UIColor(red: 211/255, green: 84/255, blue: 0/255, alpha: 1.0),
-    UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0),
-    UIColor(red: 243/255, green: 156/255, blue: 18/255, alpha: 1.0),
-    UIColor(red: 26/255, green: 188/255, blue: 156/255, alpha: 1.0),
-    UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0),
-    UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0),
-    UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0),
-    UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0),
-    UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0),
-    UIColor(red: 155/255, green: 89/255, blue: 182/255, alpha: 1.0),
-    UIColor(red: 142/255, green: 68/255, blue: 173/255, alpha: 1.0),
-    UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1.0),
-    UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0),
-    UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1.0),
-    UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1.0),
-    UIColor(red: 149/255, green: 165/255, blue: 166/255, alpha: 1.0),
-    UIColor(red: 127/255, green: 140/255, blue: 141/255, alpha: 1.0)
+    UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0),     //Alizarin
+    UIColor(red: 192/255, green: 57/255, blue: 43/255, alpha: 1.0),     //Pomegranate
+    UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1.0),    //Carrot
+    UIColor(red: 211/255, green: 84/255, blue: 0/255, alpha: 1.0),      //Pumpkin
+    UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0),    //Sun Flower
+    UIColor(red: 243/255, green: 156/255, blue: 18/255, alpha: 1.0),    //Orange
+    UIColor(red: 26/255, green: 188/255, blue: 156/255, alpha: 1.0),    //Turquoise
+    UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0),    //Green Sea
+    UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0),    //Emerald
+    UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0),     //Nephritis
+    UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0),    //Peter River
+    UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0),    //Belize Hole
+    UIColor(red: 155/255, green: 89/255, blue: 182/255, alpha: 1.0),    //Ametyst
+    UIColor(red: 142/255, green: 68/255, blue: 173/255, alpha: 1.0),    //Wisteria
+    UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1.0),      //Wet Asphalt
+    UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0),      //Midnight Blue
+    //UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1.0),  //Clouds
+    UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1.0),   //Silver
+    UIColor(red: 149/255, green: 165/255, blue: 166/255, alpha: 1.0),   //Concrete
+    UIColor(red: 127/255, green: 140/255, blue: 141/255, alpha: 1.0)    //Asbetos
 ]
